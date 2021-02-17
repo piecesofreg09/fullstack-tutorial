472d2e2c2082e2cbf756a56c5f3f127180b9dc2d
Winston Lu <luhf09@gmail.com>
2021-02-16 20:10:25 -0500

    edited

--- start/server/src/index.js
+++ start/server/src/index.js
@@ -1,1 +1,15 @@
 require('dotenv').config();
+
+const { ApolloServer } = require('apollo-server');
+const typeDefs = require('./schema');
+//console.log(typeDefs);
+const server = new ApolloServer({ typeDefs });
+
+server.listen().then(() => {
+    console.log(`
+      Server is running!
+      Listening on port 4000
+      Explore at https://studio.apollographql.com/dev
+    `);
+  });
+  
